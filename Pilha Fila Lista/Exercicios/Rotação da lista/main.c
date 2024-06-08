3b7f128b3f40ec10120067e3eae73e761274479c
Yan Menezes <pignata.yan@gmail.com>
2024-06-08 16:44:20 -0300

    Lista lincada palindrona

--- Pilha Fila Lista/Exercicios/Rotação da lista/main.c
+++ Pilha Fila Lista/Exercicios/Rotação da lista/main.c
@@ -13,11 +13,28 @@ void rotacionarLista(list *lista){
 
     //Ultimo sendo colocado na primeira posição
     lista->No = ultimoNo;
-    
+
     //Faz com que o penultimo seja o ultimo
     penultimoNo->No = NULL;
 }
 
+int verificaListaPalindromica(list *lista){
+    int array[lista->tamanhoLista];
+    int tamanhoLista = lista->tamanhoLista;
+    list *temp = lista->No;
+    for (int i = 0; i < lista->tamanhoLista && lista != NULL; i++)
+    {
+        array[i] = temp->valor;
+        temp = temp->No;
+    }
+    
+    for (int i = 0; i < tamanhoLista/2; i++)
+    {
+        if (array[i] != array[tamanhoLista - i - 1])
+            return 0;
+    }
+    return 1;
+}
 int menu(){
     int opcao;
     puts("1 - Adicionar Elemento");
@@ -25,6 +42,7 @@ int menu(){
     puts("3 - Tamanho da Lista");
     puts("4 - Remover");
     puts("5 - Rotacionar Lista ->");
+    puts("6 - E palindrona?");
     puts("0 - Sair");
     printf("Digite a opcao: ");
     scanf("%d", &opcao);
@@ -32,6 +50,7 @@ int menu(){
 }
 
 int main(){
+    int bool;
     int opcao = menu();
     list *lista = (list *)malloc(sizeof(list));
     if (lista == NULL)
@@ -62,6 +81,19 @@ int main(){
         
         case 5:
             rotacionarLista(lista);
+            break;
+
+        case 6:
+            bool = verificaListaPalindromica(lista);
+            if (bool)
+            {
+                puts("A Lista e palindrona!\n");
+            }
+            else
+            {
+                puts("A Lista nao e palindrona!\n");
+            }
+            
             break;
         
         default:
